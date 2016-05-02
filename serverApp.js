2076aa0326625c91945f201378344484fdadc717
Davi Garcia <davivcgarcia@gmail.com>
2016-05-02 09:54:16 -0300

    Fixing issues with ENV for Express Binding Port.

--- serverApp.js
+++ serverApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var htmlController = require('./controllers/htmlController');
 
 var app = express();
-var port = process.env.EXPRESS_PORT || 3000;
+var port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static(__dirname + '/public'));
