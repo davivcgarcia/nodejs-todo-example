8d155c4f2c2a100ad9a574d6a2dafb733094451c
Davi Garcia <davivcgarcia@gmail.com>
2016-05-02 17:42:02 -0300

    Implementation of CRUD APIs for Todo.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -12,4 +12,32 @@ module.exports = function(app) {
     });
   });
 
+  app.post('/api/v1/todo', function(req, res) {
+    if(req.body.id) {
+      Todos.findByIdAndUpdate(req.body.id, {
+        task: req.body.task,
+        isDone: req.body.isDone
+      }, function(err, todo) {
+        if (err) throw err;
+        res.send('Update successful!');
+      });
+    } else {
+      var newTodo = Todos({
+        task: req.body.task,
+        isDone: req.body.isDone
+      });
+      newTodo.save(function(err) {
+        if(err) throw err;
+        res.send('Creation successful!');
+      });
+    }
+  });
+
+  app.delete('/api/v1/todo', function(req, res){
+    Todos.findByIdAndRemove(req.body.id, function(err){
+      if(err) throw err;
+      res.send('Deletion successful!');
+    });
+  });
+
 };
