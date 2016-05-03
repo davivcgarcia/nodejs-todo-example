d1dfb6757b5c598cdba2ccc000f16bea60d02b91
Davi Garcia <davivcgarcia@gmail.com>
2016-05-03 12:31:32 -0300

    Fixfing JSCS warnings and adding logging middleware.

--- serverApp.js
+++ serverApp.js
@@ -1,18 +1,20 @@
 var express = require('express');
+var morgan = require('morgan');
 var mongoose = require('mongoose');
+
 var htmlController = require('./controllers/htmlController');
 var apiController = require('./controllers/apiController');
 
 var app = express();
 var port = process.env.PORT || 3000;
-var mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/nodejs-todo-example'
+var mongoUri = process.env.MONGODB_URI ||
+               'mongodb://localhost:27017/nodejs-todo-example';
 
 app.set('view engine', 'ejs');
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/assets', express.static(__dirname + '/public'));
+app.use(morgan('tiny'));
 
-mongoose.connect(mongoUri, function(error) {
-  if (error) throw error;
-});
+mongoose.connect(mongoUri);
 
 htmlController(app);
 apiController(app);
