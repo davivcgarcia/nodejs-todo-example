fc2c92f1cdeb927b7913e5c9fdc3b4a5c126d131
Davi Garcia <davivcgarcia@gmail.com>
2016-05-13 16:21:39 -0300

    Adding comments.

--- controllers/dbController.js
+++ controllers/dbController.js
@@ -1,25 +1,28 @@
 var mongoose = require('mongoose');
 
-var mongoUri = process.env.MONGODB_URI ||
-               'mongodb://mongodb:27017/nodejs-todo-example';
+var mongoUri = process.env.MONGODB_URI || 'mongodb://mongodb:27017/nodejs-todo-example';
 
 var mongoConnectionDelay = process.env.MONGODB_WAIT || 2000;
 
+// Function to encapsulate delayed connect method
 function delayedConnect(delay) {
   setTimeout(function() {
     mongoose.connect(mongoUri);
   }, delay);
 }
 
+// Event Handler for MongoDB Connection
 mongoose.connection.on('connected', function() {
   console.log('Mongoose default connection established with ' + mongoUri + '.');
 });
 
+// Event Handler for MongoDB Disconnection
 mongoose.connection.on('disconnected', function() {
   console.log('Mongoose default connection terminated. Trying to reconnect...');
   delayedConnect(mongoConnectionDelay);
 });
 
+// Event Handler for MongoDB Connection Error (with auto-reconnect)
 mongoose.connection.on('error', function(error) {
   console.log('Mongoose default connection failed: ' + error);
   mongoose.disconnect();
