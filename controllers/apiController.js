41c50069430a2aea1996b5073d1413ddc84d6ff3
Davi Garcia <davivcgarcia@gmail.com>
2016-05-13 16:22:39 -0300

    Refactoring the API endpoints for update/delete.

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ var Todos = require('../models/todoModel');
 
 module.exports = function(app) {
 
+  // API Endpoint to Get All Todos
   app.get('/api/v1/todos', function(req, res) {
     Todos.find({}, function(err, todos) {
       if (err) { throw err; }
@@ -9,9 +10,10 @@ module.exports = function(app) {
     });
   });
 
+  // API Endpoint to Create or Update a Todo
   app.post('/api/v1/todo', function(req, res) {
-    if (req.body.id) {
-      Todos.findByIdAndUpdate(req.body.id, {
+    if (req.body._id) {
+      Todos.findByIdAndUpdate(req.body._id, {
         task: req.body.task,
         isDone: req.body.isDone,
       }, function(err, todo) {
@@ -30,8 +32,21 @@ module.exports = function(app) {
     }
   });
 
+  // API Endpoint to Delete a Todo (ID in Body)
   app.delete('/api/v1/todo', function(req, res) {
-    Todos.findByIdAndRemove(req.body.id, function(err) {
+    if (req.body._id) {
+      Todos.findByIdAndRemove(req.body._id, function(err) {
+        if (err) { throw err; }
+        res.send('Deletion successful!');
+      });
+    } else {
+      res.send('Nothing to delete!');
+    }
+  });
+  
+  // API Endpoint to Delete a Todo (ID in URL)
+  app.delete('/api/v1/todo/:id', function(req, res) {
+    Todos.findByIdAndRemove(req.params.id, function(err) {
       if (err) { throw err; }
       res.send('Deletion successful!');
     });
